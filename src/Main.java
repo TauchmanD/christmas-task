28ccd8d44d87bb4659ade94c71508e4ab214f531
Denis Tauchman <denis.tauchman@biano.com>
2022-11-04 10:57:37 +0100

    sphere creation

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a  = sc.nextInt();
-        System.out.println(a);
-        //badge lmao
+        create_sphere(4, '–', 5);
+        create_sphere(4, '–', 0);
+
+    }
+    static void create_sphere(int radius, char symbol, int offset){
+        int diameter = radius*2;
+        for(int x=0; x<=diameter; x++) {
+            if(x<diameter-offset+1)
+            {
+                for (int y=0; y<=diameter; y++) {
+                    double distance = Math.sqrt(Math.pow((x-radius),2)+Math.pow((y-radius),2));
+                    if (distance > radius - 0.5 && distance < radius + 0.5) {
+                        System.out.print(symbol);
+                    } else
+                        System.out.print(" ");
+                }
+                System.out.println();
+            }
+        }
     }
 }
